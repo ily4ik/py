4e88cd0e386e20d8a85cedbf1ea38cfecefd2f01
ilya <iotarasov007l2@gmail.com>
2026-01-27 13:16:59 +0300

    Update kt1.py

--- kt1.py
+++ kt1.py
@@ -1,1 +1,15 @@
-
+import random
+counter = 0
+while(True):
+    userinput = input()
+    if (userinput == userinput.upper()):
+        if(userinput == "ПОКА!"):
+            counter += 1
+            print(f"НЕТ, НИ РАЗУ С {random.randint(1930, 1950)} ГОДА!")
+        else:
+            print(f"НЕТ, НИ РАЗУ С {random.randint(1930, 1950)} ГОДА!")
+    else:
+        print("АСЬ?! ГОВОРИ ГРОМЧЕ, ВНУЧЕК!")
+    if(counter == 3):
+        print("ДО СВИДАНИЯ, МИЛЫЙ!")
+        break
